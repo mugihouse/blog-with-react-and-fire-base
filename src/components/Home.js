2f63a6233bdf16bd98c5a88c0b9b1def28740e95
mugihouse <mugi.stnk100122@gmail.com>
2024-01-21 16:36:22 +0900

    Add: firebaseからプログ記事を読み込み表示させる

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      // data関数を使うことで中身を取り出せる
+      // スプレッド構文でカンマ区切りをつけると属性を追加できる
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div className="homepage">
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.poststext}</div>
+            <div className="nameAndDeleteButton">
+              <h3>{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
